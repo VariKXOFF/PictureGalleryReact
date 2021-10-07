d5e0ba71ab11dffa9f9a82636ec2369a7e4e1294
VariKXOFF <varikxoff@yandex.ru>
2021-10-07 17:46:00 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div className='picture-gallery'>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture1.jpeg'} alt=""/>
+            <p className='picture__text'>Цитрусовый фреш. Апельсин</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture2.jpeg'} alt=""/>
+            <p className='picture__text'>Звездная ночь</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture3.jpeg'} alt=""/>
+            <p className='picture__text'>Постояноство памяти</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture4.jpeg'} alt=""/>
+            <p className='picture__text'>Черный квадрат</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture5.jpeg'} alt=""/>
+            <p className='picture__text'>Тайная вечерня</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture6.jpeg'} alt=""/>
+            <p className='picture__text'>Герника</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture7.jpeg'} alt=""/>
+            <p className='picture__text'>Рождение Венеры</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture8.jpeg'} alt=""/>
+            <p className='picture__text'>Девочка с персиками</p>
+        </div>
+        <div className='picture'>
+            <img className='picture__image' src={process.env.PUBLIC_URL + 'pictures/picture9.jpeg'} alt=""/>
+            <p className='picture__text'>Утро в сосновом бору</p>
+        </div>
+    </div>,
+    document.querySelector('.app')
+)
